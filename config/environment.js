fd8419816ceb5acdcbcc044a558179a631a9b2e1
Alex Schiller <alexschiller@gmail.com>
2018-03-15 09:13:01 -0400

    Kick off build

--- config/environment.js
+++ config/environment.js
@@ -92,6 +92,7 @@ module.exports = function(environment) {
 
     if (environment === 'test') {
         // Testem prefers this...
+
         ENV.locationType = 'none';
 
         // keep test console output quieter
